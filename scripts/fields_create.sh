02212877e48ad6648c67e2aad04b7f843fe0c154
Hector Correa <hector_correa@brown.edu>
2019-02-06 10:45:39 -0500

    Use new field definitions

--- scripts/fields_create.sh
+++ scripts/fields_create.sh
@@ -1,46 +1,54 @@
-# Adds a few custom fields to the bibdata core
-# (author, authorsOther, authorsAll, title, title_)
+# Add support for multi-value text in english dynamic fields
 curl -X POST -H 'Content-type:application/json' --data-binary '{
-  "add-field":[
-    {
-      "name":"author",
-      "type":"string",
-      "multiValued":false
-    },
-    {
-      "name":"authorsOther",
-      "type":"string",
-      "multiValued":true
-    },
-    {
-      "name":"authorsAll",
-      "type":"text_general",
-      "multiValued":true
-    }
-  ]
+  "add-dynamic-field":{
+     "name":"*_txts_en",
+     "type":"text_en",
+     "multiValued":true}
 }' http://localhost:8983/solr/bibdata/schema
 
+# Copy title to a string field (for sorting)
 curl -X POST -H 'Content-type:application/json' --data-binary '{
   "add-copy-field":[
     {
-      "source":"author",
-      "dest":[ "authorsAll" ]
-    },
-    {
-      "source":"authorsOther",
-      "dest":[ "authorsAll" ]
+      "source":"title_txt_en",
+      "dest":[ "title_s" ]
     }
   ]
 }' http://localhost:8983/solr/bibdata/schema
 
+# Create a combined "authorsAll" field (for searching)
 curl -X POST -H 'Content-type:application/json' --data-binary '{
-  "add-field":{
-    "name":"title",
-    "type":"text_en",
-    "multiValued":false
-  },
-  "add-copy-field":{
-    "source":"title",
-    "dest":[ "title_s" ]
-  }
+  "add-copy-field":[
+    {
+      "source":"author_txt_en",
+      "dest":[ "authorsAll_txts_en" ]
+    },
+    {
+      "source":"authorsOther_txts_en",
+      "dest":[ "authorsAll_txts_en" ]
+    }
+  ]
+}' http://localhost:8983/solr/bibdata/schema
+
+
+# Copy publisher to a string field (for sorting and faceting)
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-copy-field":[
+    {
+      "source":"publisher_txt_en",
+      "dest": "publisher_s",
+      "maxChars": "100"
+    }
+  ]
+}' http://localhost:8983/solr/bibdata/schema
+
+# Copy subject to a string field (for faceting)
+curl -X POST -H 'Content-type:application/json' --data-binary '{
+  "add-copy-field":[
+    {
+      "source":"subjects_txts_en",
+      "dest": "subjects_ss",
+      "maxChars": "100"
+    }
+  ]
 }' http://localhost:8983/solr/bibdata/schema
